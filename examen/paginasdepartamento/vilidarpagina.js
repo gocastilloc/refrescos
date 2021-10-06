299333f674f913ccdbd23335f717b5766bc459ed
gocastilloc <92052234+gocastilloc@users.noreply.github.com>
2021-10-06 12:17:56 -0600

    Add files via upload

---
+++ examen/paginasdepartamento/vilidarpagina.js
@@ -0,0 +1,16 @@
+function ingresar()
+{
+
+    var usuario="gesler";
+    var contraseña="examen";
+
+    if(document.foringreso.usuario.value==usuario &&
+        document.foringreso.contraseña.value==contraseña)
+        {
+            document,location="inicio.html"
+        }
+        else
+        {
+            alert("ingrese usuario y contraceña correctamente")
+        }
+}
